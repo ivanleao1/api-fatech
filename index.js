e588b6e49287c9c1529fd2d155e0fde6282cde6d
Ivan de Oliveira leao <94879642+ivanleao1@users.noreply.github.com>
2026-06-15 21:16:59 -0300

    aula 15/06

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/alunos', (pedido, resposta) => {
 
 app.post('/alunos', (pedido, resposta) => {
     const aluno = {
+        id: alunos.length + 1,
         matricula: pedido.body.matricula,
         nome: pedido.body.nome,
         dataNasc: pedido.body.dataNasc,
@@ -23,6 +24,23 @@ app.post('/alunos', (pedido, resposta) => {
     resposta.json(aluno)
 })
 
+app.put('/alunos/:id', (pedido, resposta) => {
+    const index = alunos.findIndex(aluno => aluno.id == pedido.params.id)
+    if(index === -1) {
+        return resposta.json({mensagem: 'Aluno não encontrado!'})
+    }
+    alunos[index] = { ...alunos[index], ...pedido.body}
+    resposta.json(alunos[index])
+})
+
+app.delete('/alunos/:id', (pedido, resposta) =>{
+    const index = alunos.findIndex (aluno => aluno.id === pedido.params.id)
+    if (index === -1) {
+         return resposta.json({mensagem: 'Aluno não encontrado'})
+    }
+    alunos.splice(index, 1)
+    resposta.json({mensagem: 'Aluno deletado com sucesso!'})
+})
 
 app.listen(3000, () => {
     console.log('API rodando na porta 3000')
